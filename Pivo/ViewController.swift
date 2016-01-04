24a782f1047ce377af8f19ff8dc1a1681457a093
Veronika Vičiková <vvicikova@veronikas-macbook-pro.local>
2016-01-04 16:32:28 +0100

    Initial Commit

---
+++ Pivo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pivo
+//
+//  Created by Veronika Vičiková on 04/01/16.
+//  Copyright © 2016 Svět Solutions s.r.o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
